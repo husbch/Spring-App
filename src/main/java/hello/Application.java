d881966bebd94a45f262e28677de34d1687cd363
Husni B <husni.bakri@myinfosys.net>
2020-07-15 14:31:38 +0000

    Update Application.java 1

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World Uhh Mantaapp";
+		return "Hello Docker World Uhh Mantaapp Dongg";
 	}
 
 	public static void main(String[] args) {
