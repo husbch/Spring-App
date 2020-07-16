4d964f8d43dd35feeada571db3ea79ca75d40ce1
Husni B <husni.bakri@myinfosys.net>
2020-07-16 01:20:33 +0000

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Waahh Jadi Doonngg...";
+		return "Uhh Mantaapp";
 	}
 
 	public static void main(String[] args) {
